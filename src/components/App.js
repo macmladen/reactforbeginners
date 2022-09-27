4cef482026557f04a83652afa6f58ac61a04692a
Mladen Djuric <mladen@bluefish.rs>
2022-09-27 16:54:14 +0200

    Loading the state with sample data from the file.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -16,6 +17,11 @@ class App extends React.Component {
       fishes: fishes
     })
   }
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
         <div className="catch-of-the-day">
@@ -23,7 +29,7 @@ class App extends React.Component {
             <Header tagLine={"i bolja nego domaca"} />
           </div>
           <Order />
-          <Inventory addFish={this.addFish} />
+          <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
         </div>
     )
   }
